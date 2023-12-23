19391c220766f150fe5924621c1d00738184fffa
dmytroafr <afrosin.dmytro@gmail.com>
2023-12-23 18:15:54 +0200

    till 1.1.21

---
+++ src/Fibonacci2.java
@@ -0,0 +1,13 @@
+public class Fibonacci {
+    public static long F (int N){
+        if (N==0) return 0;
+        if (N==1) return 1;
+        return F(N-1) + F(N-2);
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i + " " + F(i));
+        }
+    }
+}
