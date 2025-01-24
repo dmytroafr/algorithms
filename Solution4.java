b68ae5284cdfda62054261d7ac3cc2a145004ae9
dmytroafr <afrosin.dmytro@gmail.com>
2025-01-24 10:01:54 +0200

    Tree symmentry solution

--- Solution4.java
+++ Solution4.java
@@ -42,9 +42,14 @@ class Solution4 {
         return root;
     }
 
-    // public boolean isSymmetric(TreeNode root) {
-        
-    // }
+    public boolean isSymmetric(TreeNode root) {
+        return isSymmetric(root.left, root.right);
+    }
+    public boolean isSymmetric(TreeNode left, TreeNode right){
+        if(left == null || right == null) return left==right;
+        if(left.val != right.val) return false;
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
     
 
 
